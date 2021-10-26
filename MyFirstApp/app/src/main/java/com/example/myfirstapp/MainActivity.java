aea04040c58906041e112bada4c5e03d02e2cf12
Hideyuki Fujikawa <hfujikawa77@gmail.com>
2021-10-27 01:24:22 +0900

    add projects (MyMAVApp, my_flutter_app)

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -7,6 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
@@ -18,10 +24,57 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(View view){
         System.out.println("Pushed!");
+        sendUdp(view);
+
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void sendUdp(View view){
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try{
+                    System.out.println("Thread send start");
+
+                    InetAddress laddr = InetAddress.getByName("10.0.2.2");
+
+
+                    DatagramSocket sendUdpSocket = new DatagramSocket();
+
+                    String localad;
+                    localad = sendUdpSocket.getLocalAddress().toString();
+                    System.out.println(localad);
+
+                    byte[] buffer = new byte[1024];
+                    buffer[1] = 0x55;
+                    buffer[2] = 0x54;
+                    buffer[3] = 0x53;
+                    buffer[4] = 0x52;
+                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length, laddr, 5000);
+                    sendUdpSocket.send(packet);
+                    System.out.println("Thread send end");
+
+                    byte[] buffer2 = new byte[1024];
+
+                    //DatagramPacket packet2 = new DatagramPacket(buffer2, buffer2.length, laddr, 5000);
+                    DatagramPacket packet2 = new DatagramPacket(buffer2, buffer2.length);
+                    sendUdpSocket.receive(packet2);
+                    String result = new String(buffer2, "UTF-8");
+                    System.out.println(result);
+
+
+
+                } catch (SocketException e){
+                    System.out.println(e.getMessage());
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }).start();
+
     }
 }
---
+++ MyMavApp/app/src/test/java/com/MAVLink/MavJavaTest.java
@@ -0,0 +1,10 @@
+package com.MAVLink;
+
+import org.junit.Test;
+
+public class MavJavaTest {
+    @Test
+    public void test(){
+        System.out.println("Called");
+    }
+}
